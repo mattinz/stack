68f9a95676719c1ab02afe616a5a7adc71e8f0e3
Mattin <mattinzargarpur@gmail.com>
2018-07-01 17:10:40 -0400

    Tile remainders now play sound on collision. Still need to adjust behavior.

---
+++ Stack/Assets/Scripts/TileCollisionSound.cs
@@ -0,0 +1,22 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+[RequireComponent(typeof(AudioSource))]
+[RequireComponent(typeof(Rigidbody))]
+public class TileCollisionSound : MonoBehaviour {
+
+	private AudioSource audioSource;
+	private Rigidbody rigidBody;
+
+	private void Awake() {
+		audioSource = GetComponent<AudioSource>();
+		rigidBody = GetComponent<Rigidbody>();
+	}
+
+	private void OnCollisionEnter(Collision collision) {
+		if(!rigidBody.isKinematic) {
+			audioSource.Play();
+		}
+	}
+}
