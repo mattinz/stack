8fbe9da474fb1229aeb4d7e340ec23198d323157
Mattin <mattinzargarpur@gmail.com>
2018-07-04 13:30:33 -0400

    Camera now zooms out on game over, zooms back in on resume.

--- Stack/Assets/Scripts/CameraMovementController.cs
+++ Stack/Assets/Scripts/CameraMovementController.cs
@@ -5,23 +5,27 @@ using UnityEngine;
 [RequireComponent(typeof(Camera))]
 public class CameraMovementController : MonoBehaviour {
 
-	[SerializeField] private float cameraSpeed = 1.0f;
+	[SerializeField] private float cameraBaseSpeed = 1.0f;
 
 	private GameState gameState;
+	private Camera camera;
 	private Vector3 startingPosition;
 	private Vector3 targetCameraPosition;
 	private float startingCameraSize;
 	private float distanceFromStack;
+	private float actualSpeed;
 
 	// Use this for initialization
 	void Start () {
 		gameState = GameObject.FindGameObjectWithTag("GameState").GetComponent<GameState>();
+		camera = GetComponent<Camera>();
 		startingPosition = transform.position;
 		targetCameraPosition = new Vector3(startingPosition.x, 0, startingPosition.z);
 		alignCameraToStackHeight();
 
-		startingCameraSize = GetComponent<Camera>().orthographicSize;
+		startingCameraSize = camera.orthographicSize;
 		distanceFromStack = Vector3.Distance(Vector3.zero, new Vector3(startingPosition.x, 0.0f, startingPosition.z));
+		actualSpeed = cameraBaseSpeed;
 	}
 	
 	// Update is called once per frame
@@ -31,17 +35,22 @@ public class CameraMovementController : MonoBehaviour {
 				//Roughly centers the stack in the camera's view.
 				float cameraHeightOffset = distanceFromStack / Mathf.Tan((90 - transform.eulerAngles.x) * 0.0174533f); // The magic number here converts degrees to radians.
 				targetCameraPosition.y = gameState.getStackHeight() / 2.0f + cameraHeightOffset;
+				actualSpeed = cameraBaseSpeed * 2.0f;
+				camera.orthographicSize = startingCameraSize * 1.5f;
 				break;
 			case GameState.State.GAME_STARTING_PHASE_TWO:
-				//Move camera in
+				alignCameraToStackHeight();
+				transform.position = targetCameraPosition;
+				camera.orthographicSize = startingCameraSize;
 				gameState.setGameState(GameState.State.GAME_RUNNING);
 				break;
 			default:
 				alignCameraToStackHeight();
+				actualSpeed = cameraBaseSpeed;
 				break;
 		}
 
-		transform.position = Vector3.MoveTowards(transform.position, targetCameraPosition, cameraSpeed * Time.deltaTime);
+		transform.position = Vector3.MoveTowards(transform.position, targetCameraPosition, actualSpeed * Time.deltaTime);
 	}
 
 	private void alignCameraToStackHeight() {
